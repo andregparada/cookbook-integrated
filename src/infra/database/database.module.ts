733573a48412e92d8a9873d99d17fe14960f0799
André Parada <andregparada@gmail.com>
2026-01-23 17:17:11 -0300

    feat: cryptography model

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { ChefsRepository } from '@/domain/application/repositories/chefs-repository'
+import { PrismaChefsRepository } from './prisma/repositories/prisma-chefs-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    { provide: ChefsRepository, useClass: PrismaChefsRepository },
+  ],
+  exports: [PrismaService, ChefsRepository],
 })
 export class DataBaseModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { DataBaseModule } from '../database/database.module'
 import { RegisterChefUseCase } from '@/domain/application/use-cases/register-chef'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
-  imports: [DataBaseModule],
+  imports: [DataBaseModule, CryptographyModule],
   controllers: [CreateAccountController],
   providers: [RegisterChefUseCase],
 })
